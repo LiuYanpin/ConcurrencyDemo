c477b8ba6782c4ca703ede79f661f2f11a5ca97c
Yanping <liuyanpingscu@163.com>
2020-07-07 23:54:23 +0800

    [Yanping] add demos

---
+++ src/main/java/JoinMain.java
@@ -0,0 +1,18 @@
+public class JoinMain {
+    public volatile static int i = 0;
+
+    public static class AddThread extends Thread {
+        @Override
+        public void run() {
+            for (i = 0; i < 10000000; i++);
+        }
+    }
+
+    public static void main(String[] args) throws InterruptedException {
+        AddThread addThread = new AddThread();
+        addThread.start();
+            System.out.println(i);
+        addThread.join();
+        System.out.println(i);
+    }
+}
